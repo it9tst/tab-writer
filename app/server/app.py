b6cc68959e9b17ac598b360763ec455a655bcd91
dariodenardi <dariodenardi@hotmail.it>
2021-02-17 18:03:28 +0100

    update server

--- app/server/app.py
+++ app/server/app.py
@@ -1,9 +1,12 @@
 import flask
 import werkzeug
 import os
+import numpy as np
 from preprocessing import preprocessing_file
-from predict import predict_model
 from flask import jsonify
+from pydub import AudioSegment
+
+from predict import predict_model
 
 app = flask.Flask(__name__)
 
@@ -19,17 +22,34 @@ def handle_request():
     print("\nReceived audio File name : " + audiofile.filename)
     audiofile.save(filename)
     
-    ffmpeg(filename)
+    try:
+        dirpath = os.path.dirname(os.path.abspath(filename))
+        filepath = dirpath + '/' + filename
+        (path, file_extension) = os.path.splitext(filepath)
+        file_extension_final = file_extension.replace('.', '')
+        track = AudioSegment.from_file(filepath, file_extension_final)
+        wav_filename = filename.replace(file_extension_final, 'wav')
+        wav_path = dirpath + '/' + wav_filename
+        print('CONVERTING: ' + str(filepath))
+        file_handle = track.export(wav_path, format='wav')
+        os.remove(filepath)
+    except:
+        print("ERROR CONVERTING")
     
-
-#    images = preprocessing_file(filename)
-#    result = predict_model(images)
-
-    return jsonify({"message":"ok", "tab": [{'tab_y': 5, 'tab_x': 0, 'value': 0}, {'tab_y': 5, 'tab_x': 1, 'value': 0}, {'tab_y': 5, 'tab_x': 2, 'value': 12}, {'tab_y': 5, 'tab_x': 3, 'value': 0}, {'tab_y': 5, 'tab_x': 4, 'value': 0}, {'tab_y': 5, 'tab_x': 5, 'value': 10}, {'tab_y': 5, 'tab_x': 6, 'value': 8}, {'tab_y': 5, 'tab_x': 7, 'value': 10}, {'tab_y': 5, 'tab_x': 8, 'value': 10}, {'tab_y': 5, 'tab_x': 9, 'value': 12}, {'tab_y': 5, 'tab_x': 10, 'value': 13}, {'tab_y': 5, 'tab_x': 11, 'value': 15}, {'tab_y': 5, 'tab_x': 12, 'value': 15}, {'tab_y': 5, 'tab_x': 13, 'value': 0}, {'tab_y': 5, 'tab_x': 14, 'value': 0}, {'tab_y': 5, 'tab_x': 15, 'value': 0}, {'tab_y': 5, 'tab_x': 16, 'value': 0}, {'tab_y': 5, 'tab_x': 17, 'value': 15}, {'tab_y': 5, 'tab_x': 18, 'value': 0}, {'tab_y': 5, 'tab_x': 19, 'value': 10}, {'tab_y': 5, 'tab_x': 20, 'value': 0}, {'tab_y': 5, 'tab_x': 21, 'value': 10}, {'tab_y': 5, 'tab_x': 22, 'value': 0}, {'tab_y': 5, 'tab_x': 23, 'value': 3}, {'tab_y': 5, 'tab_x': 24, 'value': 0}]})
-#    return jsonify({"message":"ok"})
-
-def ffmpeg(i):
-    os.system("""ffmpeg.exe -i {i} -acodec pcm_u8 -ar 44100 {i[:-4]}.wav""")
+    images = preprocessing_file(wav_filename)
+    #images = preprocessing_file(filename)
+    print(images.shape)
+    #images = np.swapaxes(images,1,2)
+    #images = np.swapaxes(images,1,3)
+    #print(images.shape)
+    #print(images)
+    #np.savetxt("nxx", images.reshape((3,-1)), fmt="%s", header=str(images.shape))
+    #result = predict_model(images)
+    #print(result)
+    
+    os.remove(wav_path)
+    
+    return jsonify(images.tolist())
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
