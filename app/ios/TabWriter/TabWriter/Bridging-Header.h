f29d0846382157adf9feaec2ba9d33234f68a72e
dariodenardi <dario.denardi@outlook.com>
2021-03-24 23:51:47 +0100

    update app ios

--- app/ios/TabWriter/TabWriter/Bridging-Header.h
+++ app/ios/TabWriter/TabWriter/Bridging-Header.h
@@ -1,13 +1,8 @@
 //
-//  Bridging-Header.h
+//  Bridging-Header.m
 //  TabWriter
 //
-//  Created by Dario De Nardi on 07/03/21.
+//  Created by Dario De Nardi on 24/03/21.
 //
 
-#ifndef Header_h
-#define Header_h
-
 #import <mobileffmpeg/MobileFFmpeg.h>
-
-#endif /* Header_h */
